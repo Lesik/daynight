273c8914080894ebf9176a9b3675143c29b4be41
Lesik <amborsdorfer@gmail.com>
2016-03-21 01:08:56 +0100

    add more modularity to daynight-gtk3.py

--- daynight-gtk3.py
+++ daynight-gtk3.py
@@ -12,6 +12,7 @@ GTK_APP_PREFER_DARK = "gtk-application-prefer-dark-theme";
 
 PREF_KEY_THEME_DARK =			"theme-dark";
 PREF_KEY_THEME_LIGHT =			"theme-light";
+PREF_KEY_THEME_DEFAULT =		"theme";
 PREF_KEY_USE_GTK_PREFER_DARK =	"use-gtk-prefer-dark";
 
 FILE_OPEN_WRITE = "w";
@@ -21,15 +22,16 @@ def daynight_gtk3(dark, preferences):
 	gtkconfig = ConfigParser();
 	gtkconfig.read(gtk_config_path);
 
-	use_gtk_prefer_dark = preferences[PREF_KEY_USE_GTK_PREFER_DARK];
+	use_gtk_prefer_dark = preferences.getboolean(PREF_KEY_USE_GTK_PREFER_DARK);
 	if use_gtk_prefer_dark:
 		gtkconfig[GTK_SETTINGS][GTK_APP_PREFER_DARK] = str(int(dark));
+		theme = preferences[PREF_KEY_THEME_DEFAULT];
 	else:
 		if dark:
 			theme = preferences[PREF_KEY_THEME_DARK];
 		else:
 			theme = preferences[PREF_KEY_THEME_LIGHT];
-		gtkconfig[GTK_PREF_THEME_NAME] = theme;
+	gtkconfig[GTK_SETTINGS][GTK_PREF_THEME_NAME] = theme;
 	
 	with open(gtk_config_path, FILE_OPEN_WRITE) as file_open:
 		gtkconfig.write(file_open);
